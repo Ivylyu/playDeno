92f27c18f9b21d495f53fe94c703b183b11799b5
Ivy <ivy.lyu@myer.com.au>
2021-03-28 11:24:13 +1100

    commit 6

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes.ts'
 const port = 5000
-
 const app = new Application()
 app.use(router.routes())
 app.use(router.allowedMethods())
